aa55783c45de976f225aa56c16580d0f668d99a0
ehu <fehuehuf@gmail.com>
2017-11-13 19:06:22 +0900

    メソッドの宣言

--- app/src/main/java/com/example/ehu/recodevoiceinbackgroud/RecordVoiceService.java
+++ app/src/main/java/com/example/ehu/recodevoiceinbackgroud/RecordVoiceService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by kaikoro on 2017/11/13.
@@ -15,4 +16,22 @@ public class RecordVoiceService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("DatabaseUpdateChecker", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("DatabaseUpdateChecker", "onDestroy");
+    }
+
 }
