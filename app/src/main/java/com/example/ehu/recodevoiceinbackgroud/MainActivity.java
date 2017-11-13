df18a55852e2e5f1da4f9d2aa0bfd143b8630040
ehu <fehuehuf@gmail.com>
2017-11-13 19:26:53 +0900

    トグルボタンとレコードクラスの実装

--- app/src/main/java/com/example/ehu/recodevoiceinbackgroud/MainActivity.java
+++ app/src/main/java/com/example/ehu/recodevoiceinbackgroud/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.ehu.recodevoiceinbackgroud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ToggleButton toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (buttonView.getId() == R.id.recordButton) {
-            Log.i("onCheckedChanged", "clicked R.id.toggleButton1");
-        } else if (buttonView.getId() == R.id.recordButton) {
-            Log.i("onCheckedChanged", "clicked R.id.toggleButton2");
-        }
+        toggle = (ToggleButton) findViewById(R.id.recordButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.d("onCheckedChanged", "R.id.toggleServiceButton is enabled");
+
+                    startService(new Intent(getBaseContext(),RecordVoiceService.class));
+                } else {
+                    Log.d("onCheckedChanged", "R.id.toggleServiceButton is disable");
+                    stopService(new Intent(getBaseContext(),RecordVoiceService.class));
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ehu/recodevoiceinbackgroud/RecordVoiceService.java
+++ app/src/main/java/com/example/ehu/recodevoiceinbackgroud/RecordVoiceService.java
@@ -2,6 +2,8 @@ package com.example.ehu.recodevoiceinbackgroud;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -11,6 +13,8 @@ import android.util.Log;
  */
 
 public class RecordVoiceService extends Service {
+    //録音クラスの宣言
+    MediaRecorder recorder;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -24,14 +28,41 @@ public class RecordVoiceService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+        startRecordVoice();
         return super.onStartCommand(intent, flags, startId);
     }
+    //録音開始
+    private void startRecordVoice(){
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
 
+        //保存先
+        String filePath = Environment.getExternalStorageDirectory() + "/audio.3gp";
+        recorder.setOutputFile(filePath);
+
+        //録音準備＆録音開始
+        try {
+            recorder.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        recorder.start();   //録音開始
+    }
+
+    //録音停止
+    private void stopRecordVoice(){
+        recorder.stop();
+        recorder.reset();   //オブジェクトのリセット
+        //release()前であればsetAudioSourceメソッドを呼び出すことで再利用可能
+        recorder.release(); //Recorderオブジェクトの解放
+    }
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d("DatabaseUpdateChecker", "onDestroy");
+        stopRecordVoice();
     }
 
 }
